9d13597a74f8996a06a528e320f025d06a626a32
Paul Ballas <paul.ballas@richmond.edu>
2019-11-11 22:17:22 -0500

    add JSON js file

---
+++ assets/javascript/characters.js
@@ -0,0 +1,35 @@
+// Creates characters
+
+var characters = [
+  {
+    Name: "Luke Skywalker",
+    HP: 120,
+    AP: 6,
+    CAP: 7,
+    imageURL: "./assets/images/Luke.jpeg"
+  },
+
+  {
+    Name: "Mace Windu",
+    HP: 120,
+    AP: 6,
+    CAP: 7,
+    imageURL: "./assets/images/Mace_Windu.png"
+  },
+
+  {
+    Name: "Darth Maul",
+    HP: 120,
+    AP: 6,
+    CAP: 7,
+    imageURL: "./assets/images/Darthmall.jpg"
+  },
+
+  {
+    Name: "Darth Vader",
+    HP: 120,
+    AP: 6,
+    CAP: 7,
+    imageURL: "./assets/images/DarthVader.jpg"
+  }
+];
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,60 @@
 // declare variables
-var lukeHP;
+var lukeHP = 150;
 var lukeAP;
 var lukeCAP;
-var maulHP;
+var maulHP = 150;
 var maulAP;
 var maulCAP;
-var obiHP;
+var obiHP = 150;
 var obiAP;
 var obiCAP;
-var vaderHP;
+var vaderHP = 150;
 var vaderAP;
 var vaderCAP;
-var sidiusHP;
-var sidiusAP;
-var sidiusCAP;
 
 $(document).ready(function() {
   // Luke Character selection
-  $(".luke").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".luke").appendTo(".character-selector");
-    $(".luke").toggleClass("pickedCharacter");
-  });
 
-  // Mace character selection
-  $(".mace").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".mace").appendTo(".character-selector");
-    $(".mace").toggleClass("pickedCharacter");
-  });
+  if ($(".player").data("clicked") !== true) {
+    console.log("fuck you");
+    $(".luke").one("click", () => {
+      console.log(event);
+      $(this).data("clicked", true);
+      $(".player").appendTo(".enemy-container");
+      $(".player").toggleClass("enemy");
+      $(".luke").appendTo(".character-selector");
+      $(".luke").toggleClass("pickedCharacter");
+    });
 
-  // Maul character Selection
-  $(".maul").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".maul").appendTo(".character-selector");
-    $(".maul").toggleClass("pickedCharacter");
-  });
+    // Mace character selection
+    $(".mace").one("click", () => {
+      $(this).data("clicked", true);
+      $(".player").appendTo(".enemy-container");
+      $(".player").toggleClass("enemy");
+      $(".mace").appendTo(".character-selector");
+      $(".mace").toggleClass("pickedCharacter");
+    });
 
-  // Obi character Selection
-  $(".obi").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".obi").appendTo(".character-selector");
-    $(".obi").toggleClass("pickedCharacter");
-  });
+    // Maul character Selection
+    $(".maul").one("click", () => {
+      $(this).data("clicked", true);
+      $(".player").appendTo(".enemy-container");
+      $(".player").toggleClass("enemy");
+      $(".maul").appendTo(".character-selector");
+      $(".maul").toggleClass("pickedCharacter");
+    });
 
-  // Maul character Selection
-  $(".vader").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".vader").appendTo(".character-selector");
-    $(".vader").toggleClass("pickedCharacter");
-  });
-
-  // Sidius character selection
-  $(".sidius").one("click", () => {
-    $(this).data("clicked", true);
-    $(".player").appendTo(".enemy-container");
-    $(".player").toggleClass("enemy");
-    $(".sidius").appendTo(".character-selector");
-    $(".sidius").toggleClass("pickedCharacter");
-  });
+    // Maul character Selection
+    $(".vader").one("click", () => {
+      $(this).data("clicked", true);
+      $(".player").appendTo(".enemy-container");
+      $(".player").toggleClass("enemy");
+      $(".vader").appendTo(".character-selector");
+      $(".vader").toggleClass("pickedCharacter");
+    });
+  } else {
+    console.log("what");
+  }
 });
 
 //   $(".mace").one("click", () => {
